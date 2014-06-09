014338de8a5363ca15b1c80100ff09b985b5bfb7
Vlad Velici <vlad.velici@gmail.com>
2014-06-09 23:52:02 +0100

    Initial CLI commit - flags defined (close #13)
    Signed-off-by: Vlad Velici <vlad.velici@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 GO SPDX Parser Library
 ======================
 
-Parser library for SPDX written in Go (golang).
+> **Note:** This is a work in progress. The above features and usage are just what is planned for the initial release.
+
+
+Parser library for SPDX written in Go (golang). It includes a CLI tool that will have features such as convert, validate and format (pretty-print).
+
+The CLI tool is a good example on how to use the /spdx, /tag and /rdf packages in other Go programs. All the features available in the CLI tool are available in the library as well.
+
+Features (planned for first release)
+------------------------------------
+
+- SPDX 1.2 (the only version supported at the moment)
+- Convert to/from rdf and tag formats
+- Validate rdf and tag formats
+- Auto-detect the input format (based on file extension)
+- Format (pretty-print) SPDX documents (rdf and tag foramts)
 
 Code
 ----
@@ -10,4 +24,3 @@ The code is available in two repositories:
 
 1. Official repository at http://git.spdx.org/spdx-tools-go.git
 2. Mirrored on GitHub at https://github.com/vladvelici/spdx-go
-
--- spdx.go
+++ spdx.go
@@ -1,9 +1,100 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const version = "pre0.0"
+const execName = "spdx-go"
+
+var specVersions = []string{"SPDX-1.2"}
+
+const (
+	formatRdf  = "rdf"
+	formatTag  = "tag"
+	formatAuto = "auto"
+)
+
+var (
+	flagOutput       = flag.String("o", "-", "Sets the output file. If not set, output is written to stdout.")
+	flagInPlace      = flag.Bool("w", false, "If defined, it overwrites the input file.")
+	flagIgnoreCase   = flag.Bool("i", false, "If defined, it ignores the case for properties. (e.g. treat \"packagename\" same as \"PackageName\")")
+	flagInputFormat  = flag.String("input", "auto", "Defines the format of the input. Valid values: rdf, tag or auto. Default is auto.")
+	flagOutputFormat = flag.String("f", "", "Mandatory on convert action, it specifies the output format (rdf or tag).")
+	flagHelp         = flag.Bool("help", false, "Show help message.")
+	flagVersion      = flag.Bool("version", false, "Show tool version and supported SPDX spec versions.")
+)
+
+var (
+	input  = os.Stdin
+	output = os.Stdout
 )
 
 func main() {
-	fmt.Println("SPDX Go Parser CLI Tool")
+	flag.Parse()
+
+	if *flagHelp {
+		help()
+		return
+	}
+
+	if *flagVersion {
+		printVersion()
+		return
+	}
+
+	if flag.NArg() < 1 {
+		log.Fatalf("Action not specified. For help, see '%s help' or '%s -help'", execName)
+	}
+
+	action := flag.Arg(0)
+
+	*flagOutputFormat = strings.ToLower(*flagOutputFormat)
+	if action == "convert" && !validFormat(*flagOutputFormat, false) {
+		log.Fatalf("No or invalid output format (-f) specified (%s). Valid values are '%s' and '%s'.", *flagOutputFormat, formatRdf, formatTag)
+	}
+
+	if !validFormat(*flagInputFormat, true) {
+		log.Fatalf("Invalid input format (-input). Valid values are '%s', '%s' and '%s'.", formatRdf, formatTag, formatAuto)
+	}
+
+	if flag.NArg() >= 2 {
+		input, err := os.Open(flag.Arg(1))
+		defer input.Close()
+		if err != nil {
+			log.Fatalf("Couldn't open input file: %s", err.Error())
+		}
+	}
+
+	if *flagOutput != "-" {
+		output, err := os.Create(*flagOutput)
+		defer output.Close()
+		if err != nil {
+			log.Fatalf("Couldn't open output file: %s", err.Error())
+		}
+	}
+}
+
+func validFormat(val string, allowAuto bool) bool {
+	return val == formatRdf || val == formatTag || (val == formatAuto && allowAuto)
+}
+
+func help() {
+	printVersion()
+
+	fmt.Printf("\nUsage: spdx-go [convert | validate | format] [<flags>] [<input file>]\n")
+	fmt.Println("Stdin is used as input if <input-file> is not specified.")
+
+	fmt.Println("\nThe following flags are available:")
+	flag.PrintDefaults()
+
+	fmt.Println("\nThe flags -help and -version do not need an action to be specified.")
+}
+
+func printVersion() {
+	fmt.Printf("spdx-go version %s.\nSupporting SPDX specifications %s.\n", version, strings.Join(specVersions, ", "))
 }
