9a4ee7773af5ebe2c98a50e5d0feb7001ed16b45
Vlad Velici <vlad.velici@gmail.com>
2014-06-25 23:54:15 +0100

    RDF Writer update
    Signed-off-by: Vlad Velici <vlad.velici@gmail.com>

---
+++ rdf/base.go
@@ -0,0 +1,50 @@
+package rdf
+
+import (
+	"github.com/vladvelici/goraptor"
+	"strings"
+)
+
+const (
+	baseUri    = "http://spdx.org/rdf/terms#"
+	licenceUri = "http://spdx.org/licenses/"
+)
+
+type pair struct {
+	key, val string
+}
+
+func prefix(k string) *goraptor.Uri {
+	var pref string
+	rest := k
+	if i := strings.Index(k, ":"); i >= 0 {
+		pref = k[:i]
+		rest = k[i+1:]
+	}
+
+	switch pref {
+	default:
+		pref = baseUri
+	case "ns":
+		pref = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	case "doap":
+		pref = "http://usefulinc.com/ns/doap#"
+	case "rdfs":
+		pref = "http://www.w3.org/2000/01/rdf-schema#"
+	}
+
+	uri := goraptor.Uri(pref + rest)
+	return &uri
+}
+
+func uri(uri string) *goraptor.Uri {
+	return (*goraptor.Uri)(&uri)
+}
+
+func literal(lit string) *goraptor.Literal {
+	return &goraptor.Literal{Value: lit}
+}
+
+func blank(b string) *goraptor.Blank {
+	return (*goraptor.Blank)(&b)
+}
--- rdf/writer.go
+++ rdf/writer.go
@@ -2,8 +2,10 @@ package rdf
 
 import (
 	"github.com/vladvelici/goraptor"
+	"github.com/vladvelici/spdx-go/spdx"
 	"io"
 	"os"
+	"strconv"
 )
 
 // Writes the input to the specified RDF format
@@ -30,29 +32,143 @@ func WriteRdf(input io.Reader, output io.Writer, format string) error {
 // Used to write documents in RDF format
 type Formatter struct {
 	serializer *goraptor.Serializer
+	nodeIds    map[string]int
 }
 
 // Create a new Formatter that writes to output
 func NewFormatter(output *os.File, format string) *Formatter {
 	s := goraptor.NewSerializer(format)
-	s.StartStream(output, "")
-	return &Fromatter{
+	s.StartStream(output, baseUri)
+	return &Formatter{
 		serializer: s,
+		nodeIds:    make(map[string]int),
 	}
 }
 
-// Write a document.
-func (f *Formatter) Document(doc *spdx.Document) error {
+// Create a new node id for the given prefix
+func (f *Formatter) newId(prefix string) *goraptor.Blank {
+	f.nodeIds[prefix]++
+	id := goraptor.Blank(prefix + strconv.Itoa(f.nodeIds[prefix]))
+	return &id
+}
+
+// Sets the type t to node
+func (f *Formatter) setType(node goraptor.Term, t string) error {
+	return f.add(node, prefix("ns:type"), prefix(t))
+}
+
+// Add `key`=`value` at object `to`.
+func (f *Formatter) add(to, key, value goraptor.Term) error {
+	return f.serializer.Add(&goraptor.Statement{
+		Subject:   to,
+		Predicate: key,
+		Object:    value,
+	})
+}
+
+// Using the SPDX baseUri, add a goraptor.Term.
+func (f *Formatter) addTerm(to goraptor.Term, key string, value goraptor.Term) error {
+	return f.add(to, prefix(key), value)
+}
+
+// Using the SPDX baseUri, add pairs of literals.
+func (f *Formatter) addPairs(to goraptor.Term, pairs ...pair) error {
+	for _, p := range pairs {
+		if err := f.addLiteral(to, p.key, p.val); err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+// Using the SPDX baseUri, add one literal. Does not write anything and returns nil if the value is empty.
+func (f *Formatter) addLiteral(to goraptor.Term, key, value string) error {
+	if value == "" {
+		return nil
+	}
+	return f.add(to, prefix(key), &goraptor.Literal{Value: value})
+}
+
+// Write a document.
+func (f *Formatter) Document(doc *spdx.Document) (docId goraptor.Term, err error) {
+	_docId := goraptor.Blank("doc")
+	docId = &_docId
+
+	if err = f.setType(docId, "SpdxDocument"); err != nil {
+		return
+	}
+
+	if err = f.addLiteral(docId, "specVersion", doc.SpecVersion.Val); err != nil {
+		return
+	}
+
+	if err = f.addTerm(docId, "dataLicense", uri(licenceUri+doc.DataLicence.Val)); err != nil {
+		return
+	}
+
+	if id, err := f.CreationInfo(doc.CreationInfo); err == nil {
+		if err = f.addTerm(docId, "creationInfo", id); err != nil {
+			return docId, err
+		}
+	} else {
+		return docId, err
+	}
+
+	if err = f.addLiteral(docId, "rdfs:comment", doc.Comment.Val); err != nil {
+		return
+	}
+
+	/*
+		if err = f.Reviews(docId, "reviewed", doc.Reviews); err != nil {
+			return
+		}
+	*/
+	if err = f.Packages(docId, "describesPackage", doc.Packages); err != nil {
+		return
+	}
+
+	/*
+	   if err = f.Files(docId, "referencesFile", doc.Files); err != nil {
+	       return
+	   }
+
+	   if err = f.ExtrLicInfos(docId, "hasExtractedLicensingInfo", doc.ExtractedLicenceInfo); err != nil {
+	       return
+	   }
+	*/
+
+	return docId, nil
 }
 
 // Write creation info.
-func (f *Formatter) CreationInfo(cr *spdx.CreationInfo) error {
-	return nil
+func (f *Formatter) CreationInfo(cr *spdx.CreationInfo) (id goraptor.Term, err error) {
+	id = f.newId("cri")
+
+	if err = f.setType(id, "CreationInfo"); err != nil {
+		return
+	}
+
+	err = f.addPairs(id,
+		pair{"created", cr.Created.V()},
+		pair{"rdfs:comment", cr.Comment.V()},
+		pair{"licenseListVersion", cr.LicenceListVersion.V()},
+	)
+
+	if err != nil {
+		return
+	}
+
+	for _, creator := range cr.Creator {
+		if err = f.addLiteral(id, "creator", creator.V()); err != nil {
+			return
+		}
+	}
+
+	return id, nil
 }
 
 // Write a slice of reviews.
-func (f *Formatter) Reviews(rs *[]spdx.Review) error {
+func (f *Formatter) Reviews(rs []*spdx.Review) error {
 	for _, r := range rs {
 		if err := f.Review(r); err != nil {
 			return err
@@ -67,9 +183,16 @@ func (f *Formatter) Review(r *spdx.Review) error {
 }
 
 // Write a slice of packages.
-func (f *Formatter) Packages(pkgs []*spdx.Package) error {
+func (f *Formatter) Packages(parent goraptor.Term, element string, pkgs []*spdx.Package) error {
+	if len(pkgs) == 0 {
+		return nil
+	}
 	for _, pkg := range pkgs {
-		if err := f.Package(pkg); err != nil {
+		pkgid, err := f.Package(pkg)
+		if err != nil {
+			return err
+		}
+		if err = f.addTerm(parent, element, pkgid); err != nil {
 			return err
 		}
 	}
@@ -77,12 +200,67 @@ func (f *Formatter) Packages(pkgs []*spdx.Package) error {
 }
 
 // Write a package.
-func (f *Formatter) Package(pkg *spdx.Package) error {
-	return nil
+func (f *Formatter) Package(pkg *spdx.Package) (id goraptor.Term, err error) {
+	id = f.newId("pkg")
+
+	if err = f.setType(id, "Package"); err != nil {
+		return
+	}
+
+	err = f.addPairs(id,
+		pair{"name", pkg.Name.Val},
+		pair{"versionInfo", pkg.Version.Val},
+		pair{"packageFileName", pkg.FileName.Val},
+		pair{"supplier", pkg.Supplier.V()},
+		pair{"originator", pkg.Originator.V()},
+		pair{"downloadLocation", pkg.DownloadLocation.Val},
+		pair{"doap:homepage", pkg.HomePage.Val},
+		pair{"sourceInfo", pkg.SourceInfo.Val},
+		pair{"licenseComments", pkg.LicenceComments.Val},
+		pair{"copyrightText", pkg.CopyrightText.Val},
+		pair{"summary", pkg.Summary.Val},
+		pair{"description", pkg.Description.Val},
+	)
+	if err != nil {
+		return
+	}
+
+	if pkg.VerificationCode != nil {
+		pkgid, err := f.VerificationCode(pkg.VerificationCode)
+		if err != nil {
+			return id, err
+		}
+		if err = f.addTerm(id, "packageVerificationCode", pkgid); err != nil {
+			return id, err
+		}
+	}
+	return id, nil
 }
 
-// Write a slice of ExtractedLicenceInfo
-func (f *Formatter) ExtrLicInfos(lics []*spdx.ExtractedLicenceInfo) error {
+func (f *Formatter) VerificationCode(vc *spdx.VerificationCode) (id goraptor.Term, err error) {
+	id = f.newId("vc")
+
+	if err = f.setType(id, "PackageVerificationCode"); err != nil {
+		return
+	}
+
+	err = f.addLiteral(id, "packageVerificationCodeValue", vc.Value.Val)
+	if err != nil {
+		return
+	}
+
+	for _, excl := range vc.ExcludedFiles {
+		err = f.addLiteral(id, "packageVerificationCodeExcludedFile", excl.Val)
+		if err != nil {
+			return
+		}
+	}
+
+	return id, nil
+}
+
+// Write a slice of ExtractedLicensingInfo
+func (f *Formatter) ExtrLicInfos(lics []*spdx.ExtractedLicensingInfo) error {
 	for _, lic := range lics {
 		if err := f.ExtrLicInfo(lic); err != nil {
 			return err
@@ -91,8 +269,8 @@ func (f *Formatter) ExtrLicInfos(lics []*spdx.ExtractedLicenceInfo) error {
 	return nil
 }
 
-// Write an ExtractedLicenceInfo
-func (f *Formatter) ExtrLicInfo(lic *spdx.ExtractedLicenceInfo) error {
+// Write an ExtractedLicensingInfo
+func (f *Formatter) ExtrLicInfo(lic *spdx.ExtractedLicensingInfo) error {
 	return nil
 }
 
@@ -114,5 +292,5 @@ func (f *Formatter) File(file *spdx.File) error {
 // Closes the stream and frees the serializer. Always call after writing using the Formatter.
 func (f *Formatter) Close() {
 	f.serializer.EndStream()
-	f.serializer.Close()
+	f.serializer.Free()
 }
